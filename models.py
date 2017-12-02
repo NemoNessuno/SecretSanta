efdd0247e04690a28e65efa54c1b59776bc4cd99
Sören Titze <4thTry@gmail.com>
2017-12-02 13:28:29 +0100

    New Datamodel

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+import enum as enum
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Date, Enum, Table
 from sqlalchemy.orm import validates, relationship
 from db_handler import Base
 from password import Password
@@ -8,15 +10,8 @@ class User(Base):
     __tablename__ = 'users'
     email = Column(String(120), unique=True, primary_key=True)
     password = Column(Password)
-    contribution = Column('contribution_id',
-                          ForeignKey('contributions.contribution_id'))
-    description_id = Column(Integer,
-                            ForeignKey('descriptions.description_id'))
-    description = relationship("Description", foreign_keys=[description_id])
-    other_description_id = Column(Integer,
-                                  ForeignKey('descriptions.description_id'))
-    other_description = relationship("Description",
-                                     foreign_keys=[other_description_id])
+    admin = Column(Boolean)
+
     # Or specify a cost factor other than the default 12
     # password = Column(Password(rounds=10))
 
@@ -44,31 +39,69 @@ class User(Base):
         self.validated = False
 
     def __repr__(self):
-        return '<User %r>' % (self.email)
+        return '<User %r>' % self.email
 
 
-class Contribution(Base):
-    __tablename__ = 'contributions'
-    id = Column('contribution_id', Integer, primary_key=True)
-    description = Column(String(120), nullable=False)
+class Round(Base):
+    __tablename__ = 'rounds'
+    id = Column(Integer, primary_key=True)
+    running = Column(Boolean)
+    created_at = Column(Date)
 
-    def __init__(self, description=None):
+    def __init__(self, running=True, created_at=datetime.now):
+        self.running = running
+        self.created_at = created_at
+
+
+class Participation(Base):
+    __tablename__ = 'participations'
+    id = Column(Integer, primary_key=True)
+    description_id = Column(Integer, ForeignKey('descriptions.id'))
+    description = relationship("Description", foreign_keys=[description_id])
+    other_description_id = Column(Integer, ForeignKey('descriptions.id'))
+    other_description = relationship("Description", foreign_keys=[other_description_id])
+    eligible = Column(Boolean)
+
+    def __init__(self, description=None, other_description=None, eligible=False):
         self.description = description
+        self.other_description = other_description
+        self.eligible = eligible
+
+
+description_questions = Table(
+    'association', Base.metadata,
+    Column('description_id', Integer, ForeignKey('description.id')),
+    Column('question_id', Integer, ForeignKey('question.id'))
+)
 
 
 class Description(Base):
     __tablename__ = 'descriptions'
-    id = Column('description_id', Integer, primary_key=True)
-    person = Column(String(256), nullable=False)
-    place = Column(String(256), nullable=False)
-    quote = Column(String(256), nullable=False)
-    theme = Column(String(256), nullable=False)
-    saturday = Column(String(256), nullable=False)
+    id = Column('id', Integer, primary_key=True)
+    user_id = Column(String(120), ForeignKey('user.email'))
+    user = relationship("User", foreign_keys=[user_id])
+    questions = relationship("Question", secondary=description_questions)
 
-    def __init__(self, person=None, place=None,
-                 quote=None, theme=None, saturday=None):
-        self.person = person
-        self.place = place
-        self.quote = quote
-        self.theme = theme
-        self.saturday = saturday
+    def __init__(self, user=None, questions=None):
+        if questions is None:
+            questions = []
+        self.user = user
+        self.questions = questions
+
+
+class QuestionType(enum.Enum):
+    text = 1,
+    picture = 2,
+    sound = 3
+
+
+class Question(Base):
+    __tablename__ = 'questions'
+    id = Column('id', Integer, primary_key=True)
+    text = Column(String(256))
+    q_type = Column('type', Enum(QuestionType))
+
+    def __init__(self, text=None, q_type=QuestionType.text):
+        self.text = text
+        self.q_type = q_type
+
